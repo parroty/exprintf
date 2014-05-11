118076d8d112a2ca563bf4d45018a083734fb998
parroty <parroty00@gmail.com>
2014-05-11 22:20:08 +0900

    Remove file

--- package.exs
+++
@@ -1,4 +0,0 @@
-Expm.Package.new(name: "exprintf", description: "A printf / sprintf library for Elixir",
-                 version: "0.0.1", keywords: ["Elixir","printf","sprintf", "io.format"],
-                 maintainers: [[name: "parroty", email: "parroty00@gmail.com"]],
-                 repositories: [[github: "parroty/exprintf"]])
