dbd8e5ff7c771898af994a71fddff16804ae9234
parroty <parroty00@gmail.com>
2014-05-28 08:21:26 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExPrintf.Mixfile do
 
   def project do
     [ app: :exprintf,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 0.13.3",
       deps: deps,
       description: description,
